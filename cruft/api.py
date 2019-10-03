b13cf9205ad2c247530331196cb2debdea6bb8c9
Peter Olson <peter.olson@gmail.com>
2019-10-02 21:45:40 -0700

    Tab out code that doesn't need with section

--- cruft/api.py
+++ cruft/api.py
@@ -147,101 +147,101 @@ def update(
     with open(cruft_file) as cruft_open_file:
         cruft_state = json.load(cruft_open_file)
 
-        if toml and os.path.isfile(pyproject_file):
-            pyproject_cruft = toml.load(pyproject_file).get("tool", {}).get("cruft", {})
-            cruft_state.setdefault("skip", []).extend(pyproject_cruft.get("skip", []))
+    if toml and os.path.isfile(pyproject_file):
+        pyproject_cruft = toml.load(pyproject_file).get("tool", {}).get("cruft", {})
+        cruft_state.setdefault("skip", []).extend(pyproject_cruft.get("skip", []))
 
-        with TemporaryDirectory() as compare_directory:
-            template_dir = os.path.join(compare_directory, "template")
+    with TemporaryDirectory() as compare_directory:
+        template_dir = os.path.join(compare_directory, "template")
 
-            try:
-                repo = Repo.clone_from(cruft_state["template"], template_dir)
-                last_commit = repo.head.object.hexsha
-            except Exception as e:  # pragma: no cover
-                raise InvalidCookiecutterRepository(e)
+        try:
+            repo = Repo.clone_from(cruft_state["template"], template_dir)
+            last_commit = repo.head.object.hexsha
+        except Exception as e:  # pragma: no cover
+            raise InvalidCookiecutterRepository(e)
 
-            if last_commit == cruft_state["commit"] or not repo.index.diff(cruft_state["commit"]):
-                return False
+        if last_commit == cruft_state["commit"] or not repo.index.diff(cruft_state["commit"]):
+            return False
 
-            context_file = os.path.join(template_dir, "cookiecutter.json")
+        context_file = os.path.join(template_dir, "cookiecutter.json")
 
-            new_output_dir = os.path.join(compare_directory, "new_output")
+        new_output_dir = os.path.join(compare_directory, "new_output")
 
-            new_context = _generate_output(
-                context_file=context_file,
-                cruft_state=cruft_state,
-                cookiecutter_input=cookiecutter_input,
-                template_dir=template_dir,
-                output_dir=new_output_dir,
-            )
+        new_context = _generate_output(
+            context_file=context_file,
+            cruft_state=cruft_state,
+            cookiecutter_input=cookiecutter_input,
+            template_dir=template_dir,
+            output_dir=new_output_dir,
+        )
 
-            old_output_dir = os.path.join(compare_directory, "old_output")
-            repo.head.reset(commit=cruft_state["commit"], working_tree=True)
-            _generate_output(
-                context_file=context_file,
-                cruft_state=cruft_state,
-                cookiecutter_input=cookiecutter_input,
-                template_dir=template_dir,
-                output_dir=old_output_dir,
-            )
+        old_output_dir = os.path.join(compare_directory, "old_output")
+        repo.head.reset(commit=cruft_state["commit"], working_tree=True)
+        _generate_output(
+            context_file=context_file,
+            cruft_state=cruft_state,
+            cookiecutter_input=cookiecutter_input,
+            template_dir=template_dir,
+            output_dir=old_output_dir,
+        )
 
-            main_directory: str = ""
-            for file_name in os.listdir(old_output_dir):
-                file_path = os.path.join(old_output_dir, file_name)
+        main_directory: str = ""
+        for file_name in os.listdir(old_output_dir):
+            file_path = os.path.join(old_output_dir, file_name)
+            if os.path.isdir(file_path):
+                main_directory = file_name
+
+        new_main_directory = os.path.join(new_output_dir, main_directory)
+        old_main_directory = os.path.join(old_output_dir, main_directory)
+
+        for skip_file in cruft_state.get("skip", []):
+            file_path_old = os.path.join(old_main_directory, skip_file)
+            file_path_new = os.path.join(new_main_directory, skip_file)
+            for file_path in (file_path_old, file_path_new):
                 if os.path.isdir(file_path):
-                    main_directory = file_name
+                    rmtree(file_path)
+                elif os.path.isfile(file_path):
+                    os.remove(file_path)
 
-            new_main_directory = os.path.join(new_output_dir, main_directory)
-            old_main_directory = os.path.join(old_output_dir, main_directory)
+        diff = run(
+            ["git", "diff", "--no-index", old_main_directory, new_main_directory],
+            stdout=PIPE,
+            stderr=PIPE,
+        ).stdout.decode("utf8")
+        diff = diff.replace(old_main_directory, "").replace(new_main_directory, "")
 
-            for skip_file in cruft_state.get("skip", []):
-                file_path_old = os.path.join(old_main_directory, skip_file)
-                file_path_new = os.path.join(new_main_directory, skip_file)
-                for file_path in (file_path_old, file_path_new):
-                    if os.path.isdir(file_path):
-                        rmtree(file_path)
-                    elif os.path.isfile(file_path):
-                        os.remove(file_path)
+        print("The following diff would be applied:\n")
+        print(diff)
+        print("")
 
-            diff = run(
-                ["git", "diff", "--no-index", old_main_directory, new_main_directory],
-                stdout=PIPE,
-                stderr=PIPE,
-            ).stdout.decode("utf8")
-            diff = diff.replace(old_main_directory, "").replace(new_main_directory, "")
+        if not skip_apply_ask and not skip_update:  # pragma: no cover
+            update: str = ""
+            while update not in ("y", "n", "s"):
+                print(
+                    'Respond with "s" to intentionally skip the update while marking '
+                    "your project as up-to-date."
+                )
+                update = input("Apply diff and update [y/n/s]? ").lower()  # nosec
 
-            print("The following diff would be applied:\n")
-            print(diff)
-            print("")
+            if update == "n":
+                sys.exit("User cancelled Cookiecutter template update.")
+            elif update == "s":
+                skip_update = True
 
-            if not skip_apply_ask and not skip_update:  # pragma: no cover
-                update: str = ""
-                while update not in ("y", "n", "s"):
-                    print(
-                        'Respond with "s" to intentionally skip the update while marking '
-                        "your project as up-to-date."
-                    )
-                    update = input("Apply diff and update [y/n/s]? ").lower()  # nosec
+        current_directory = os.getcwd()
+        try:
+            os.chdir(expanded_dir)
+            if not skip_update:
+                run(["patch", "--merge"], input=diff.encode("utf8"))
 
-                if update == "n":
-                    sys.exit("User cancelled Cookiecutter template update.")
-                elif update == "s":
-                    skip_update = True
+            cruft_state["commit"] = last_commit
+            cruft_state["context"] = new_context
+            with open(cruft_file, "w") as cruft_output:
+                json_dump(cruft_state, cruft_output)
+        finally:
+            os.chdir(current_directory)
 
-            current_directory = os.getcwd()
-            try:
-                os.chdir(expanded_dir)
-                if not skip_update:
-                    run(["patch", "--merge"], input=diff.encode("utf8"))
-
-                cruft_state["commit"] = last_commit
-                cruft_state["context"] = new_context
-                with open(cruft_file, "w") as cruft_output:
-                    json_dump(cruft_state, cruft_output)
-            finally:
-                os.chdir(current_directory)
-
-            return True
+        return True
 
 
 @example("https://github.com/timothycrosley/cookiecutter-python/", no_input=True, use_latest=True)
