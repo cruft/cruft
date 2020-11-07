d25deb7b71eeb5631f86c499446927756f01a859
Sambhav Kothari <sambhavs.email@gmail.com>
2020-11-07 22:25:38 +0000

    Fix cruft update and change the diff order
    This commit provides the following fixes -
    
    https://github.com/cruft/cruft/issues/67
    Fixes cruft not detecting new files
    
    https://github.com/cruft/cruft/issues/53
    Fixes cruft not being able to handle moves
    
    Fixes cruft diff order so that the base repo is the project repo.
    This way any changes from the upstream template can be outputted to
    a diff file and applied if needed.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-./scripts/lint.sh
 poetry run pytest -s -n auto --cov=cruft/ --cov=tests --cov-report=term-missing ${@-} --cov-report xml --cov-report html
+./scripts/lint.sh
