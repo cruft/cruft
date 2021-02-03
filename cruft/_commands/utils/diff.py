d9947121e8c8b92b08930266e8ea522ada3e9052
Jens Hübner <jhuebner@nano.uni-hannover.de>
2021-02-03 14:54:44 +0100

    used posix paths for win and nix OSs

--- cruft/_commands/utils/diff.py
+++ cruft/_commands/utils/diff.py
@@ -9,9 +9,12 @@ def _git_diff(*args: str) -> List[str]:
 
 def get_diff(repo0: Path, repo1: Path) -> str:
     """Compute the raw diff between two repositories."""
+    # Use Path methods in order to straighten out the differeneces between the the OSs.
+    repo0_str = repo0.resolve().as_posix()
+    repo1_str = repo1.resolve().as_posix()
     diff = run(
-        _git_diff("--no-ext-diff", "--no-color", str(repo0), str(repo1)),
-        cwd=str(repo0),
+        _git_diff("--no-ext-diff", "--no-color", repo0_str, repo1_str),
+        cwd=repo0_str,
         stdout=PIPE,
         stderr=PIPE,
     ).stdout.decode()
@@ -23,8 +26,17 @@ def get_diff(repo0: Path, repo1: Path) -> str:
     # Note that diff headers contain repo0 and repo1 with both "a" and "b"
     # prefixes: headers for new files have a/repo1, headers for deleted files
     # have b/repo0.
-    for repo in [repo0, repo1]:
-        diff = diff.replace("a" + str(repo), "a").replace("b" + str(repo), "b")
+    # NIX OPs have a/folder/file
+    # WIN OPS have a/c:/folder/file
+    for repo in [repo0_str, repo1_str]:
+        if repo[0] == '/':
+            # NIX case
+            diff = diff.replace("a" + repo, "a").replace("b" + repo, "b")
+        else:
+            # WIN case
+            diff = diff.replace("a/" + repo, "a").replace("b/" + repo, "b")
+
+
     # This replacement is needed for renamed/moved files to be recognized properly
     # Renamed files in the diff don't have the "a" or "b" prefix and instead look like
     # /tmp/tmpmp34g21y/remote/.coveragerc
@@ -32,10 +44,11 @@ def get_diff(repo0: Path, repo1: Path) -> str:
     # we would end up with /.coveragerc which doesn't work.
     # We also need to replace the trailing slash. As a result, we only do
     # this after the above replacement is made as the trailing slash is needed there.
-    diff = diff.replace(str(repo0) + "/", "").replace(str(repo1) + "/", "")
+    diff = diff.replace(repo0_str + "/", "").replace(repo1_str + "/", "")
+
     return diff
 
 
 def display_diff(repo0: Path, repo1: Path):
-    """Displays the diff between two repositories."""
-    run(_git_diff(str(repo0), str(repo1)))
+    """Display the diff between two repositories."""
+    run(_git_diff(repo0.as_posix(), repo1.as_posix()))
