24b36713bdf8991ea453370579c33653adf92e0d
Sambhav Kothari <sambhavs.email@gmail.com>
2021-11-07 19:17:46 +0000

    Update _version.py

--- cruft/_version.py
+++ cruft/_version.py
@@ -1,1 +1,8 @@
-__version__ = "2.9.1"
+from importlib_metadata import PackageNotFoundError, version
+
+# Used to automatically set version number from github actions
+# as well as not break when being tested locally
+try:
+    __version__ = version(__package__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"
