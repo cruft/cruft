7c63ae4678023926f3c022cf4e21be415520b865
Philipp Sommer <philipp.sommer@hereon.de>
2023-03-24 09:57:59 +0100

    implement support for local extensions via AltTemporaryDirectory
    as implemented in https://github.com/MichalKubek/cruft/commit/5e3ca7585f9f7c6577e7f6522f32948e8745f74d

--- cruft/_commands/utils/iohelper.py
+++ cruft/_commands/utils/iohelper.py
@@ -1,15 +1,23 @@
 from tempfile import TemporaryDirectory
 from time import sleep
+import sys
 
 
 class AltTemporaryDirectory:
     def __init__(self):
         self.tmpdir = TemporaryDirectory()
+        self._extended_path = False
+        name = self.tmpdir.name
+        if name not in sys.path:
+            self._extended_path = True
+            sys.path.append(name)
 
     def __enter__(self):
         return self.tmpdir.name
 
     def cleanup(self, cnt=0):
+        if self._extended_path:
+            sys.path.remove(self.tmpdir.name)
         if cnt >= 5:  # pragma: no cover
             raise RuntimeError("Could not delete TemporaryDirectory!")
         try:
