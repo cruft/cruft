27e4f9a4ac831a7364877aac7d66d63d86a44875
Philipp Sommer <philipp.sommer@hereon.de>
2023-03-29 08:03:08 +0200

    minor fix for update if repo is there is no directory

--- cruft/_commands/update.py
+++ cruft/_commands/update.py
@@ -38,7 +38,13 @@ def update(
 
     cruft_state = json.loads(cruft_file.read_text())
 
-    with AltTemporaryDirectory(cruft_state.get("directory")) as tmpdir_:
+    directory = cruft_state.get("directory", "")
+    if directory:
+        directory = str(Path("repo") / directory)
+    else:
+        directory = "repo"
+
+    with AltTemporaryDirectory(directory) as tmpdir_:
         # Initial setup
         tmpdir = Path(tmpdir_)
         repo_dir = tmpdir / "repo"
