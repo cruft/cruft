ec127f1cf7ef0e7a11dcbeb19fca575d789ed18d
Sambhav Kothari <sambhavs.email@gmail.com>
2024-12-25 00:15:50 +0000

    Update project to python 3.8 and fix CI and move to ruff (#321)

--- cruft/__init__.py
+++ cruft/__init__.py
@@ -3,6 +3,7 @@
 Allows you to maintain all the necessary cruft for packaging and building projects separate from
 the code you intentionally write. Built on-top of, and fully compatible with, CookieCutter.
 """
+
 from cruft._commands import check, create, diff, link, update
 from cruft._version import __version__
 
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -2,12 +2,5 @@
 set -euxo pipefail
 
 poetry run cruft check
-poetry run mypy --ignore-missing-imports cruft/
-poetry run isort --check --diff cruft/ tests/
-poetry run black --check cruft/ tests/
-poetry run flake8 cruft/ tests/
-# Safety won't allow installations for tornado<=6.1
-# but tornade 6.1 is the latest.
-# Run safety checks with warnings only
-poetry run safety check -i 39462 -i 40291 -i 47794 -i 50571 || true
-poetry run bandit -r cruft/
+poetry run ruff check cruft/ tests/
+poetry run ruff format --check cruft/ tests/
